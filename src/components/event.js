82c10869e8031d80379b06d3a5ad5ded5225a3d1
Margarita <margaritk@inbox.ru>
2020-05-02 19:02:51 +0300

    Создаёт компоненты

---
+++ src/components/event.js
@@ -0,0 +1,72 @@
+import moment from 'moment';
+import util from '../base/util.js';
+import AbstractComponent from './abstract-component.js';
+
+const MS_IN_DAY = 86400000;
+const MS_IN_HOUR = 3600000;
+
+export default class Event extends AbstractComponent {
+  constructor(events) {
+    super();
+    this._type = events.type;
+    this._city = events.city;
+    this._options = events.type.options;
+    this._timeStart = events.timeStart;
+    this._timeEnd = events.timeEnd;
+    this._duration = events.timeEnd - events.timeStart;
+    this._price = events.price;
+  }
+
+  getTemplate() {
+    return `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${this._type.name}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${util.getEventTitle(this._type.name)} ${this._city.name}</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="${this._timeStart ? `${moment(this._timeStart).format()}` : ``}">${this._timeStart ? `${moment(this._timeStart).format(`HH:mm`)}` : ``}</time>
+            &mdash;
+            <time class="event__end-time" datetime="${moment(this._timeEnd).format()}">${moment(this._timeEnd).format(`HH:mm`)}</time>
+          </p>
+          <p class="event__duration">${this._getDuration()}</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${this._price}</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${this._options ? `${this._options.filter((option) => option.isChecked).map(({description, cost}) => `<li class="event__offer">
+            <span class="event__offer-title">${description}</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">${cost}</span>
+          </li>`
+          .trim())
+          .join(``)}` : ``}
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>`.trim();
+  }
+
+  _getDuration() {
+    let duration;
+    if (this._duration >= MS_IN_DAY) {
+      const numberOfDays = util.getNumberWithZero(Math.floor(this._duration / MS_IN_DAY));
+      duration = `${numberOfDays}D ${moment.utc(this._duration).format(`HH[H] mm[M]`)}`;
+    } else if (this._duration < MS_IN_DAY && this._duration >= MS_IN_HOUR) {
+      duration = moment.utc(this._duration).format(`HH[H] mm[M]`);
+    } else {
+      duration = moment.utc(this._duration).format(`mm[M]`);
+    }
+
+    return duration;
+  }
+}
---
+++ src/components/trip-day-item.js
@@ -0,0 +1,40 @@
+import constant from '../base/constant.js';
+import AbstractComponent from './abstract-component.js';
+
+const DEFAULT_DAY_COUNT = 1;
+const LetterNumber = {
+  FIRST_OF_YEAR: 8,
+  LAST_OF_YEAR: 10,
+  FIRST_OF_MONTH: 4,
+  LAST_OF_MONTH: 7,
+  FIRST_OF_DAY: 11,
+  LAST_OF_DAY: 16,
+  FIRST_OF_MONTH_DAY: 4,
+  LAST_OF_MONTH_DAY: 11,
+};
+
+
+export default class TripDayItem extends AbstractComponent {
+  constructor(eventsCount = DEFAULT_DAY_COUNT, day, index) {
+    super();
+    this._day = day;
+    this._eventsCount = eventsCount;
+    this._index = index;
+  }
+
+  getTemplate() {
+    return `<li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">${this._index === 0 || this._index ? (this._index + 1) : ``}</span>
+        <time class="day__date" datetime="
+        ${this._day ? `${this._day.slice(LetterNumber.FIRST_OF_DAY, LetterNumber.LAST_OF_DAY)}-${constant.monthMap[this._day.slice(LetterNumber.FIRST_OF_MONTH, LetterNumber.LAST_OF_MONTH)]}-${this._day.slice(LetterNumber.FIRST_OF_YEAR, LetterNumber.LAST_OF_YEAR)}` : ``}">
+          ${this._day ? `${this._day.slice(LetterNumber.FIRST_OF_MONTH_DAY, LetterNumber.LAST_OF_MONTH_DAY)}` : ``}
+        </time>
+      </div>
+
+      <ul class="trip-events__list">
+        ${new Array(this._eventsCount).fill(``).map(() => `<li class="trip-events__item"></li>`.trim()).join(``)}
+      </ul>
+    </li>`.trim();
+  }
+}
